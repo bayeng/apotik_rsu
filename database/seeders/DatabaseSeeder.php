e1ad7795ed088b1e9f65ebc9e2c43c5ac3cd9852
bayeng <88835193+bayeng@users.noreply.github.com>
2024-06-09 10:04:19 +0700

    add: create user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
 //            'email' => 'test@example.com',
 //        ]);
         $this->call([
-            SuplierSeeder::class
+            SuplierSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use function Laravel\Prompts\table;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -13,8 +16,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB:table('users')->insert([
-            'nama'=>st
-    ]);
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('users')->insert([
+                'nama' => $faker->name,
+                'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+                'tempat_lahir' => $faker->city,
+                'tgl_lahir' => $faker->date(),
+                'gol_darah' => $faker->randomElement(['A', 'B', 'AB', 'O']),
+                'agama' => $faker->randomElement(['Islam', 'Kristen', 'Hindu', 'Budha', 'Konghucu']),
+                'alamat' => $faker->address,
+                'notlp' => $faker->phoneNumber,
+                'nip' => $faker->unique()->numberBetween(1000000000, 9999999999),
+                'username' => $faker->unique()->userName,
+                'password' => Hash::make('123'),
+                'role' => $faker->randomElement(['ADMIN', 'PEGAWAI']),
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'remember_token' => \Illuminate\Support\Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
