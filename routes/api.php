1762e240cbf2230e03b2e32ae581bcef9a66d603
khoican <88487817+khoican@users.noreply.github.com>
2024-06-12 00:13:51 +0700

    fix: routes of supliers

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/users', \App\Http\Controllers\Api\UserController::class);
 
-Route::apiResource('/suplier', \App\Http\Controllers\Api\SuplierController::class);
+Route::apiResource('/supliers', \App\Http\Controllers\Api\SuplierController::class);
 
 Route::apiResource('/obats', \App\Http\Controllers\Api\ObatContoller::class);
 
