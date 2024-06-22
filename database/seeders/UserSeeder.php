a15a1abd6acd6b6bb297f67000aa1003be23d602
bayeng <88835193+bayeng@users.noreply.github.com>
2024-06-22 20:36:45 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,46 @@ class UserSeeder extends Seeder
     {
         $faker = Faker::create();
 
+        DB::table('users')->insert([
+            'nama' => $faker->name,
+            'jenis_kelamin' => $faker->boolean,
+            'tempat_lahir' => $faker->city,
+            'tgl_lahir' => $faker->date(),
+            'gol_darah' => $faker->randomElement(['A', 'B', 'AB', 'O']),
+            'agama' => $faker->randomElement(['Islam', 'Kristen', 'Hindu', 'Budha', 'Konghucu']),
+            'alamat' => $faker->address,
+            'notlp' => $faker->phoneNumber,
+            'nip' => $faker->unique()->numberBetween(1000000000, 9999999999),
+            'username' => 'admin',
+            'password' => Hash::make('123'),
+            'role' => $faker->randomElement(['ADMIN', 'PEGAWAI']),
+//                'email' => $faker->unique()->safeEmail,
+//                'email_verified_at' => now(),
+            'remember_token' => \Illuminate\Support\Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => $faker->name,
+            'jenis_kelamin' => $faker->boolean,
+            'tempat_lahir' => $faker->city,
+            'tgl_lahir' => $faker->date(),
+            'gol_darah' => $faker->randomElement(['A', 'B', 'AB', 'O']),
+            'agama' => $faker->randomElement(['Islam', 'Kristen', 'Hindu', 'Budha', 'Konghucu']),
+            'alamat' => $faker->address,
+            'notlp' => $faker->phoneNumber,
+            'nip' => $faker->unique()->numberBetween(1000000000, 9999999999),
+            'username' => 'pegawai',
+            'password' => Hash::make('123'),
+            'role' => $faker->randomElement(['ADMIN', 'PEGAWAI']),
+//                'email' => $faker->unique()->safeEmail,
+//                'email_verified_at' => now(),
+            'remember_token' => \Illuminate\Support\Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
         foreach (range(1, 20) as $index) {
             DB::table('users')->insert([
                 'nama' => $faker->name,
